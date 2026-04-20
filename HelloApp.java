8ba10c1f6f127e93c605854b5db90a924c50fdb8
vedhpatel18 <halpanivedh189@gmail.com>
2026-04-20 09:45:33 +0530

    UC2 Added

---
+++ HelloApp.java
@@ -0,0 +1,16 @@
+/* 
+@version 2.0
+@author Vedh
+*/
+import java.util.Scanner;
+
+public class HelloApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
