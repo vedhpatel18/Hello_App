3dc13dc489d4085207db0ff5a2eb266b13a7e99e
vedhpatel18 <halpanivedh189@gmail.com>
2026-04-20 09:54:10 +0530

    UC3 Added

---
+++ HelloApp.java
@@ -0,0 +1,14 @@
+/* 
+@version 3.0
+@author Vedh
+*/
+public class HelloApp{
+    public static void main(String[] args) {
+String name = "World";
+
+if (args.length > 0) {
+    name = args[0];
+}
+System.out.println("Hello, " + name + "!");
+}
+}
