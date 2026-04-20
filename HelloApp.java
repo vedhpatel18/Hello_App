b5999ba564c6c23eaf063f266372abe5854d2dcc
vedhpatel18 <halpanivedh189@gmail.com>
2026-04-20 09:14:59 +0530

    Main Branch Added

---
+++ HelloApp.java
@@ -0,0 +1,13 @@
+/* 
+@version 1.0
+@author Vedh
+ */
+class HelloApp{
+	public static void main(String[] args ){
+		
+		String msg = "This is Main Branch";
+		
+		System.out.println(msg);
+		
+	}
+}
