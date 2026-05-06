d3fc6b9c14788fd9f8128bde7d3f64f60238f7f2
vedhpatel18 <halpanivedh189@gmail.com>
2026-05-06 08:58:41 +0530

    UC5 Added

---
+++ HelloApp.java
@@ -0,0 +1,21 @@
+/* 
+@version 5.0
+@author Vedh
+*/
+public class HelloApp{
+    public static void main (String[] args){
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+        for (String name : args){
+            if (! first){
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+        if (args.length == 0){
+        System.out.println("Hello, World!");
+        return;}
+        System.out.println("Hello " + nameBuilder + "!");
+    }
+}
