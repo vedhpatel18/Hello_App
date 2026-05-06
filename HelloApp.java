7391bec0aae2c6017fa66c38a1fde908026c63d0
vedhpatel18 <halpanivedh189@gmail.com>
2026-05-06 08:50:40 +0530

    UC7 Added

--- HelloApp.java
+++ HelloApp.java
@@ -6,7 +6,7 @@ public class HelloApp {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello, World!");
+            System.out.println("Hello , World!");
         } 
         else {
             String names = String.join(", ", args);
