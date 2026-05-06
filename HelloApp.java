df96126b172fce776172508b8f838820a64e1ef3
vedhpatel18 <halpanivedh189@gmail.com>
2026-05-06 08:59:43 +0530

    UC6 Added

---
+++ HelloApp.java
@@ -0,0 +1,20 @@
+/* 
+@version 6.0
+@author Vedh
+ */
+public class HelloApp {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+            String names = "";
+            for (String name : args) {
+                names += name + ", ";
+            }
+            names = names.substring(0, names.length() - 2);
+            System.out.println("Hello " + names);
+        }
+    }
+}
